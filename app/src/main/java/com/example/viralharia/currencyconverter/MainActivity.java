96e5f117e864473be27e1856e434c67595f76e97
jarvis905 <jarvis905@users.noreply.github.com>
2019-08-18 00:10:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viralharia/currencyconverter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.viralharia.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void convert(View view)  {
+        EditText dollarfield = (EditText) findViewById(R.id.dollarfield);
+        Double dollarAmount = Double.parseDouble(dollarfield.getText().toString());
+        Double inrAmount = dollarAmount * 70.0;
+        Toast.makeText(getApplicationContext(), "INR " + inrAmount.toString(), Toast.LENGTH_LONG).show();
+    }
+
+}
